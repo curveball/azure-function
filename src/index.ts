c613a2195d68731ec47b96303a6ac32cb4c61345
Evert Pot <me@evertpot.com>
2020-03-18 23:43:40 -0400

    Lint and bugfixes

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
-import { Application, Context, MemoryRequest, MemoryResponse } from '@curveball/core';
+import { Application, Context, MemoryRequest, MemoryResponse, Response } from '@curveball/core';
 import qs from 'querystring';
-import { AzureFunction, Context as AzureContext, HttpRequest as AzureRequest } from "@azure/functions"
+import { AzureFunction, Context as AzureContext, HttpRequest as AzureRequest } from '@azure/functions'
 import { AzureResponse } from './types';
 
 export default function lambdaHandler(app: Application): AzureFunction {
 
-  const httpTrigger: AzureFunction = async function (azrContext: AzureContext, request: AzureRequest): Promise<void> {
+  const httpTrigger: AzureFunction = async (azrContext: AzureContext, request: AzureRequest): Promise<void> => {
 
     const context = new Context(
       azureRequestToCurveball(request),
-      new MemoryResponse,
+      new MemoryResponse(),
     )
 
     await app.handle(context);
@@ -30,17 +30,17 @@ function azureRequestToCurveball(request: AzureRequest): MemoryRequest<any> {
 
   return new MemoryRequest(
     request.method,
-    request.url + '?' + qs.stringify(req.query),
+    request.url + '?' + qs.stringify(request.query),
     request.headers,
     request.rawBody
   );
 
 }
 
-function curveballResponseToAzure(cbResponse: MemoryResponse<any>): AzureResponse {
+function curveballResponseToAzure(cbResponse: Response<any>): AzureResponse {
 
   let isRaw = false;
-  let body = cbResponse.body;
+  const body = cbResponse.body;
 
   if (body instanceof Buffer) {
     isRaw = true;
