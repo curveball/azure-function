640c570e28d792ce1e0a1d8adb2b7934458b001c
Evert Pot <me@evertpot.com>
2023-02-13 23:05:58 -0500

    Support Curveball origin features

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,13 @@ import * as qs from 'querystring';
 import { AzureFunction, Context as AzureContext, HttpRequest as AzureRequest } from '@azure/functions';
 import { AzureResponse } from './types';
 
-export default function lambdaHandler(app: Application): AzureFunction {
+export default function azureHandler(app: Application): AzureFunction {
 
   const httpTrigger: AzureFunction = async (azrContext: AzureContext, request: AzureRequest): Promise<void> => {
 
     const context = new Context(
-      azureRequestToCurveball(request),
-      new MemoryResponse(),
+      azureRequestToCurveball(app.origin, request),
+      new MemoryResponse(app.origin),
     );
 
     await app.handle(context);
@@ -22,13 +22,14 @@ export default function lambdaHandler(app: Application): AzureFunction {
 
 }
 
-function azureRequestToCurveball(request: AzureRequest): MemoryRequest<any> {
+function azureRequestToCurveball(origin: string, request: AzureRequest): MemoryRequest<any> {
 
   if (request.method === null) {
     throw new Error('Azure method can not be null');
   }
 
   return new MemoryRequest(
+    origin,
     request.method,
     request.url + '?' + qs.stringify(request.query),
     request.headers,
